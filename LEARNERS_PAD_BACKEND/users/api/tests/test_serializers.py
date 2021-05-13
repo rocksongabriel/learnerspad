a8836d37ebcfb957804bbb34372ea663c185572d
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-13 11:53:24 +0000

    refactor: Grouped the developer user serializers under one class

--- LEARNERS_PAD_BACKEND/users/api/tests/test_serializers.py
+++ LEARNERS_PAD_BACKEND/users/api/tests/test_serializers.py
@@ -1,93 +1,99 @@
 import factory
 import pytest
-from ...tests.factories import DeveloperUserFactory
-from ..serializers import DeveloperUserLoginSerializer, DeveloperUserRegistrationSerializer, DeveloperUserRetrieveSerializer, DeveloperUserUpdateSerializer
-import factory
-from rest_framework.test import APIClient, APIRequestFactory
-from rest_framework.request import Request
 from django.urls import reverse
+from rest_framework.request import Request
+from rest_framework.test import APIClient, APIRequestFactory
+
+from ...tests.factories import DeveloperUserFactory
+from ..serializers import (DeveloperUserLoginSerializer,
+                           DeveloperUserRegistrationSerializer,
+                           DeveloperUserRetrieveSerializer,
+                           DeveloperUserUpdateSerializer)
 
 
-@pytest.mark.django_db
-def test_serialization__developer_user_registration_serializer():
-    user = DeveloperUserFactory.build()
-    serializer = DeveloperUserRegistrationSerializer(user)
+pytestmark = pytest.mark.django_db
 
-    assert serializer.data
+class TestDeveloperUserSerializers:
+    
+    def test_serialization__developer_user_registration_serializer(self):
+        user = DeveloperUserFactory.build()
+        serializer = DeveloperUserRegistrationSerializer(user)
 
-@pytest.mark.django_db
-def test_deserialization__developer_user_registration_serializer():
-    json_data = factory.build(
-        dict,
-        FACTORY_CLASS = DeveloperUserFactory
-    )
+        assert serializer.data
 
-    serializer = DeveloperUserRegistrationSerializer(data=json_data)
+    
+    def test_deserialization__developer_user_registration_serializer(self):
+        json_data = factory.build(
+            dict,
+            FACTORY_CLASS = DeveloperUserFactory
+        )
 
-    assert serializer.is_valid()
-    assert serializer.errors == {}
+        serializer = DeveloperUserRegistrationSerializer(data=json_data)
 
-@pytest.mark.django_db
-def test_serialization__developer_user_retrieve_serializer():
-    user = DeveloperUserFactory.build()
+        assert serializer.is_valid()
+        assert serializer.errors == {}
 
-    url = reverse("users:developer-user-detail", kwargs={"username": user.username})
+    
+    def test_serialization__developer_user_retrieve_serializer(self):
+        user = DeveloperUserFactory.build()
 
-    context = {
-        "request": Request(APIRequestFactory().get(url))
-    }
+        url = reverse("users:developer-user-detail", kwargs={"username": user.username})
 
-    serializer = DeveloperUserRetrieveSerializer(instance=user, context=context)
+        context = {
+            "request": Request(APIRequestFactory().get(url))
+        }
 
-    assert serializer.data
+        serializer = DeveloperUserRetrieveSerializer(instance=user, context=context)
 
-@pytest.mark.django_db
-def test_deserialization__developer_user_retrieve_serializer():
-    json_data = factory.build(
-        dict,
-        FACTORY_CLASS = DeveloperUserFactory,
-    )
+        assert serializer.data
 
-    serializer = DeveloperUserRetrieveSerializer(data=json_data)
+    
+    def test_deserialization__developer_user_retrieve_serializer(self):
+        json_data = factory.build(
+            dict,
+            FACTORY_CLASS = DeveloperUserFactory,
+        )
 
-    assert serializer.is_valid()
-    assert serializer.errors == {}
+        serializer = DeveloperUserRetrieveSerializer(data=json_data)
 
-@pytest.mark.django_db
-def test_serialization__developer_user_update_serializer():
-    user = DeveloperUserFactory.build()
+        assert serializer.is_valid()
+        assert serializer.errors == {}
 
-    serializer = DeveloperUserUpdateSerializer(user)
+    
+    def test_serialization__developer_user_update_serializer(self):
+        user = DeveloperUserFactory.build()
 
-    assert serializer.data
+        serializer = DeveloperUserUpdateSerializer(user)
 
-@pytest.mark.django_db
-def test_deserialization__developer_user_update_serializer():
-    json_data = factory.build(
-        dict,
-        FACTORY_CLASS = DeveloperUserFactory
-    )
+        assert serializer.data
 
-    serializer = DeveloperUserUpdateSerializer(data=json_data)
+    
+    def test_deserialization__developer_user_update_serializer(self):
+        json_data = factory.build(
+            dict,
+            FACTORY_CLASS = DeveloperUserFactory
+        )
 
-    assert serializer.is_valid()
-    assert serializer.errors == {}
+        serializer = DeveloperUserUpdateSerializer(data=json_data)
 
-@pytest.mark.django_db
-def test_serialization__developer_user_login_serializer():
-    user = DeveloperUserFactory.build()
-    serializer = DeveloperUserLoginSerializer(user)
+        assert serializer.is_valid()
+        assert serializer.errors == {}
 
-    assert serializer.data
+    
+    def test_serialization__developer_user_login_serializer(self):
+        user = DeveloperUserFactory.build()
+        serializer = DeveloperUserLoginSerializer(user)
 
-@pytest.mark.django_db
-def test_deserialization__developer_user_login_serializer():
-    json_data = factory.build(
-        dict,
-        FACTORY_CLASS=DeveloperUserFactory
-    )
+        assert serializer.data
 
-    serializer = DeveloperUserLoginSerializer(data=json_data)
+    
+    def test_deserialization__developer_user_login_serializer(self):
+        json_data = factory.build(
+            dict,
+            FACTORY_CLASS=DeveloperUserFactory
+        )
 
-    assert serializer.is_valid()
-    assert serializer.errors == {}
+        serializer = DeveloperUserLoginSerializer(data=json_data)
+
+        assert serializer.is_valid()
+        assert serializer.errors == {}

76f112f4e4eaea6f8c84f924e88b6b054a7dda83
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-13 01:26:03 +0000

    Add serializer to retrieve developer user data and associated test for checking the serialization

--- LEARNERS_PAD_BACKEND/users/api/serializers.py
+++ LEARNERS_PAD_BACKEND/users/api/serializers.py
@@ -3,7 +3,21 @@ from ..models import DeveloperUser
 
 
 class DeveloperUserRegistrationSerializer(serializers.HyperlinkedModelSerializer):
-    
+
     class Meta:
         model = DeveloperUser
         fields = ["username", "email", "password"]
+
+
+class DeveloperUserRetrieveSerializer(serializers.HyperlinkedModelSerializer):
+
+    url = serializers.HyperlinkedIdentityField(
+        view_name="users:developer-user-detail",
+        source="username",
+        lookup_field="username",
+        lookup_url_kwarg="username"
+    )
+
+    class Meta:
+        model = DeveloperUser
+        fields = ["id", "url", "username", "email", "first_name", "last_name"]
--- LEARNERS_PAD_BACKEND/users/api/tests/test_serializers.py
+++ LEARNERS_PAD_BACKEND/users/api/tests/test_serializers.py
@@ -1,8 +1,11 @@
 import factory
 import pytest
 from ...tests.factories import DeveloperUserFactory
-from ..serializers import DeveloperUserRegistrationSerializer
+from ..serializers import DeveloperUserRegistrationSerializer, DeveloperUserRetrieveSerializer
 import factory
+from rest_framework.test import APIClient, APIRequestFactory
+from rest_framework.request import Request
+from django.urls import reverse
 
 
 @pytest.mark.django_db
@@ -23,3 +26,17 @@ def test_deserialization__developer_user_registration_serializer():
 
     assert serializer.is_valid()
     assert serializer.errors == {}
+
+@pytest.mark.django_db
+def test_serialization__developer_user_retrieve_serializer():
+    user = DeveloperUserFactory.build()
+
+    url = reverse("users:developer-user-data", {"username", user.username})
+
+    context = {
+        "request": Request(APIRequestFactory().get(url))
+    }
+
+    serializer = DeveloperUserRetrieveSerializer(instance=user, context=context)
+
+    assert serializer.data

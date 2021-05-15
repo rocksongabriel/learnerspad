7f95065845a5e90deca8c0551821a999ee7a6c93
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-15 16:36:57 +0000

    Register User models to the admin interface

--- LEARNERS_PAD_BACKEND/users/admin.py
+++ LEARNERS_PAD_BACKEND/users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
+from .models import DeveloperUser, StudentUser, User
 
-# Register your models here.
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "first_name", "last_name", "type", "last_login"]
+    readonly_fields = ["username", "email", "first_name", "last_name", "type", "last_login", "password", "is_staff", "is_superuser"]
+
+@admin.register(User)
+class UserAdmin(UserModelAdmin):
+    pass
+
+
+@admin.register(DeveloperUser)
+class DeveloperUserAdmin(UserModelAdmin):
+    pass
+
+
+@admin.register(StudentUser)
+class StudentUserAdmin(UserModelAdmin):
+    pass

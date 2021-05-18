8b186d78eb8f3bd6dda8cdf3c205faf95ffe47d9
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-18 22:16:36 +0000

    Add user login url and move token urls to users app urls

--- LEARNERS_PAD_BACKEND/users/api/urls.py
+++ LEARNERS_PAD_BACKEND/users/api/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from ..views import (DeveloperUserLoginView, DeveloperUserRegisterView,
                      DeveloperUserRetrieveView, StudentUserLoginView,
-                     StudentUserRegisterView, StudentUserRetrieveView)
+                     StudentUserRegisterView, StudentUserRetrieveView,
+                     UserLoginAPIView)
 
 urlpatterns = [
     path("developer/retrieve/<str:username>/", DeveloperUserRetrieveView.as_view(), name="developer-user-detail"),
@@ -12,4 +13,6 @@ urlpatterns = [
     path("student/retrieve/<str:username>/", StudentUserRetrieveView.as_view(), name="student-user-detail"),
     path("student/create/", StudentUserRegisterView.as_view(), name="student-user-create"),
     path("student/login/", StudentUserLoginView.as_view(), name="student-user-login"),
+
+    path("login/", UserLoginAPIView.as_view(), name="login"),
 ]

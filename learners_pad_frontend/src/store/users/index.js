a5a01b88d93a7a9ebfac0f48469077bfbd80e97c
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-18 23:38:36 +0000

    Complete the login functionality in frontend

--- learners_pad_frontend/src/store/users/index.js
+++ learners_pad_frontend/src/store/users/index.js
@@ -1,25 +1,33 @@
-import axios from "axios";
+import { AUTH_HTTP } from "../../../http-common";
 
 const state = {
-  userData: {},
+  userData: {}, // hold the initial data from login
+  errorMessages: {}, // hold error messages
 };
 
 const actions = {
   async login({ commit }, form) {
     // log the user in
-    let url = "http://localhost:8000/api/users/account/developer/login/"; // change this url and make it generic
-    const res = await axios.post(url, form);
-
-    // commit data to state
-    commit("UPDATE_USER_DATA", res.data);
-
-    console.log(res);
+    let url = "api/users/account/login/";
+    await AUTH_HTTP.post(url, form)
+      .then((res) => {
+        // commit data to state
+        commit("UPDATE_USER_DATA", res.data);
+      })
+      .catch((error) => {
+        commit("UPDATE_ERROR_MESSAGES", error.response.data);
+      });
   },
 };
 
 const mutations = {
   UPDATE_USER_DATA(state, payload) {
+    state.errorMessages = {}; // set the errors to an empty object
     state.userData = payload;
+  },
+  UPDATE_ERROR_MESSAGES(state, payload) {
+    state.userData = {}; // set the initial user data to an empty object
+    state.errorMessages = payload;
   },
 };
 

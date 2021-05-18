7f8e9d7e63e261336bf763f5fe8dd4f13bcfb937
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-18 06:42:21 +0000

    Add CORS middleware to the middles

--- LEARNERS_PAD_BACKEND/LEARNERSPAD/config/settings/base.py
+++ LEARNERS_PAD_BACKEND/LEARNERSPAD/config/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -129,7 +130,7 @@ REST_FRAMEWORK = {
 }
 
 # CORS settings
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
     # TODO - set this properly for production

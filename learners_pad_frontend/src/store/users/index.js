b2bbf6f8c2050b5feeaebf2ae8ca3913b0230632
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-18 04:36:16 +0000

    Add users store module

---
+++ learners_pad_frontend/src/store/users/index.js
@@ -0,0 +1,16 @@
+const state = {};
+
+const actions = {};
+
+const mutations = {};
+
+const getters = {};
+
+const usersModule = {
+  state,
+  actions,
+  mutations,
+  getters,
+};
+
+export default usersModule;

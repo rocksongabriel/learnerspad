0365d8814a12f992197d56e2311cdf18ad3a386d
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-18 04:37:56 +0000

    Set up routes

--- learners_pad_frontend/src/store/index.js
+++ learners_pad_frontend/src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import usersModule from "@/store/users/index.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: {
+    usersModule,
+  },
 });

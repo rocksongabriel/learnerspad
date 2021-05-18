63960ad53ba334cc286f4631ff4e3d8cd2219a7e
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-18 04:57:33 +0000

    Add login form structure

--- learners_pad_frontend/src/views/Login.vue
+++ learners_pad_frontend/src/views/Login.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     <h1>Login</h1>
+    <form @submit.prevent>
+      <label for="username1">Username</label> <br />
+      <input type="text" name="usrname" id="username1" /> <br />
+
+      <label for="password1">Password</label> <br />
+      <input type="password" name="password" id="password1" /> <br />
+
+      <button>Login</button>
+      <div>
+        <a href="#">Create account</a> | <a href="#">Forgot Password</a>
+      </div>
+    </form>
   </div>
 </template>
 

a7bfcf68186240c52bbb37bafe01c921932cf8b4
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-20 22:40:48 +0000

    Add the vuex persisted state, which makes the state persistent in the code

--- learners_pad_frontend/src/store/index.js
+++ learners_pad_frontend/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import usersModule from "@/store/users/index.js";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
@@ -8,4 +9,5 @@ export default new Vuex.Store({
   modules: {
     usersModule,
   },
+  plugins: [createPersistedState()],
 });

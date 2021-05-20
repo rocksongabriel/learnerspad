bc7f21842c6de8431a15e9b46cdea91b1d57ce63
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-20 22:42:09 +0000

    Add redirection to dashboard if the user is authenticated and visits the site

--- learners_pad_frontend/src/App.vue
+++ learners_pad_frontend/src/App.vue
@@ -30,4 +30,31 @@
   </div>
 </template>
 
+<script>
+/* eslint-disable */
+
+import { mapGetters } from "vuex";
+import router from "@/router/";
+
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters([
+      "isAuthenticated",
+    ])
+  },
+  mounted() {
+    // automatically send the user to the dashboard if they  are logged in
+    if (this.isAuthenticated) {
+      if (this.$route.name !== "Dashboard") {
+        router.push({name: "Dashboard"});
+      }
+    }
+  }
+};
+</script>
+
 <style></style>

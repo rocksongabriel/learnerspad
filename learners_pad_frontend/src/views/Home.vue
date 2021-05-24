fd5dff192a10a4d18a96f24354387cb314a0b878
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-24 22:22:41 +0000

    Make the website related links children of the home view and load them here"

--- learners_pad_frontend/src/views/Home.vue
+++ learners_pad_frontend/src/views/Home.vue
@@ -1,12 +1,44 @@
 <template>
   <div class="home">
-    <h1>Homepage of learnerspad</h1>
+    <!-- Navbar -->
+    <Navbar />
+
+    <!-- Content of the homepage -->
+    <div class="mx-4 md:mx-8 lg:mx-16" v-show="$route.path == '/'">
+      <h1
+        class="
+          text-6xl
+          font-bold font-sans
+          md:text-8xl
+          lg:text-9xl
+          text-center
+          py-4
+          lg:py-10
+        "
+      >
+        Wanna Learn Faster, Use LearnersPad to Achieve That
+      </h1>
+    </div>
+
+    <!-- Website related content will be loaded here -->
+    <div class="w-full" style="min-height: 80vh">
+      <router-view />
+    </div>
+
+    <!-- Footer -->
+    <Footer />
   </div>
 </template>
 
 <script>
+import Navbar from "../components/website/Navbar";
+import Footer from "../components/website/Footer";
+
 export default {
   name: "Home",
-  components: {},
+  components: {
+    Navbar,
+    Footer,
+  },
 };
 </script>

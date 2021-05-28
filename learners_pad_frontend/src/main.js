4da340cb1a4f7eb8e83a4ad72d5d1499e880de8d
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-28 11:59:54 +0000

    Register the fontawesome library

--- learners_pad_frontend/src/main.js
+++ learners_pad_frontend/src/main.js
@@ -4,6 +4,18 @@ import router from "./router";
 import store from "./store";
 import "./assets/tailwind.css";
 import axios from "axios";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+  faDev,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faGithub, faLinkedin, faTwitter, faDev);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 // Make Axios play nice with Django CSRF
 axios.defaults.xsrfCookieName = "csrftoken";

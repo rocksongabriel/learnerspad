d89945f203b5f2de35cffdec94245c4e90594151
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-06-05 18:01:08 +0000

    Add editor component and register it in the NoteTaking view

---
+++ learners_pad_frontend/src/components/dashboard/note-taking/Editor.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <vue-editor v-model="content" />
+  </div>
+</template>
+
+<script>
+import { VueEditor } from "vue2-editor";
+
+export default {
+  name: "Editor",
+
+  components: {
+    VueEditor,
+  },
+
+  data() {
+    return {
+      content: "This is the content",
+    };
+  },
+};
+</script>
--- learners_pad_frontend/src/views/dashboard/NoteTaking.vue
+++ learners_pad_frontend/src/views/dashboard/NoteTaking.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    <h1>Note Taking View</h1>
+    <h1 class="text-center text-red-600 font-bold text-2xl">Notes</h1>
+
+    <Editor />
   </div>
 </template>
 
 <script>
+import Editor from "../../components/dashboard/note-taking/Editor";
+
 export default {
   name: "NoteTaking",
-}
+
+  components: {
+    Editor,
+  },
+
+  data() {
+    return {};
+  },
+};
 </script>

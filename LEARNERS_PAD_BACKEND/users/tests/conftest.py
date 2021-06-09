bb8e54eba8e1a194d61f5e67229ef3fa1c57a234
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-06-09 09:03:33 +0000

    Refactor: Create a developer user fixture to be passed to the tests of developer object creation

--- LEARNERS_PAD_BACKEND/users/tests/conftest.py
+++ LEARNERS_PAD_BACKEND/users/tests/conftest.py
@@ -1,7 +1,18 @@
 import pytest
+from .factories import DeveloperUserFactory
+from .utils import get_user_data
 
 
+# APIClient fixture
 @pytest.fixture
 def api_client():
     from rest_framework.test import APIClient
     return APIClient()
+
+
+# Developer User object fixture
+@pytest.fixture
+def developer_user():
+    generated_user = DeveloperUserFactory.build()
+    user_data = get_user_data(generated_user)
+    return user_data
--- LEARNERS_PAD_BACKEND/users/tests/unit/test_models.py
+++ LEARNERS_PAD_BACKEND/users/tests/unit/test_models.py
@@ -8,25 +8,16 @@ pytestmark = pytest.mark.django_db
 
 class TestUserObjectCreation:
 
-    def test_single_developer_user_object_creation(self):
-        generated_user = DeveloperUserFactory.build()
-        user_data = get_user_data(generated_user)
-        DeveloperUser.objects.create(**user_data)
-
+    def test_single_developer_user_object_creation(self, developer_user):
+        DeveloperUser.objects.create(**developer_user)
         assert DeveloperUser.objects.count() == 1
 
-    def test_created_developer_user_has_specified_data(self):
-        generated_user = DeveloperUserFactory.build()
-        user_data = get_user_data(generated_user)
-        user = DeveloperUser.objects.create(**user_data)
+    def test_created_developer_user_has_specified_data(self, developer_user):
+        user = DeveloperUser.objects.create(**developer_user)
+        assert user.username == developer_user["username"]
 
-        assert user.username == generated_user.username
-
-    def test_type_of_created_developer_user(self):
-        generated_user = DeveloperUserFactory.build()
-        user_data = get_user_data(generated_user)
-        user = DeveloperUser.objects.create(**user_data)
-
+    def test_type_of_created_developer_user(self, developer_user):
+        user = DeveloperUser.objects.create(**developer_user)
         assert user.type == "DEVELOPER"
 
 
